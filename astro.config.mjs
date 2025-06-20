97e83948c3ef89ba0599090923dc6a1a7abd5a5e
HarshCasper <erbeusgriffincasper@gmail.com>
2025-06-20 09:37:54 +0530

    add link validator

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig, envField } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightUtils from '@lorenzo_lewis/starlight-utils';
+import starlightLinksValidator from 'starlight-links-validator'
 
 import markdoc from '@astrojs/markdoc';
 
@@ -60,6 +61,9 @@ export default defineConfig({
         alt: 'LocalStack',
       },
       plugins: [
+        starlightLinksValidator({
+          errorOnRelativeLinks: false,
+        }),
         starlightUtils({
           multiSidebar: {
             switcherStyle: 'dropdown',
