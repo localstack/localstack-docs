52ba2b605419b8eb65e412494f6ce1f3ba602e61
Brian Rinaldi <brian.rinaldi@gmail.com>
2025-06-27 08:13:11 -0400

    Left hand navigation work
    Global styles, fonts and left hand nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig, envField } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightUtils from '@lorenzo_lewis/starlight-utils';
 import starlightDocSearch from '@astrojs/starlight-docsearch';
-import starlightLinksValidator from 'starlight-links-validator'
-import starlightImageZoom from 'starlight-image-zoom'
+import starlightLinksValidator from 'starlight-links-validator';
+import starlightImageZoom from 'starlight-image-zoom';
 import sitemap from '@astrojs/sitemap';
-import starlightFullViewMode from 'starlight-fullview-mode'
-import starlightTocOverviewCustomizer from 'starlight-toc-overview-customizer'
+import starlightFullViewMode from 'starlight-fullview-mode';
+import starlightTocOverviewCustomizer from 'starlight-toc-overview-customizer';
 
 import markdoc from '@astrojs/markdoc';
 
@@ -43,7 +43,11 @@ export default defineConfig({
     starlight({
       title: 'Docs',
       favicon: '/images/favicons/favicon.ico',
-      customCss: ['./src/styles/global.css'],
+      customCss: [
+        './src/fonts/font-face.css',
+        './src/styles/global.css',
+        './src/styles/custom.css',
+      ],
       editLink: {
         baseUrl: 'https://github.com/localstack/localstack-docs/edit/master/',
       },
@@ -80,9 +84,11 @@ export default defineConfig({
             'data-website-id': '3dfbd0ac-9e56-4664-8315-032e17917ab6',
             'data-project-name': 'LocalStack',
             'data-project-color': '#281763',
-            'data-project-logo': 'https://avatars.githubusercontent.com/u/28732122?s=280&v=4',
+            'data-project-logo':
+              'https://avatars.githubusercontent.com/u/28732122?s=280&v=4',
             'data-user-analytics-fingerprint-enabled': 'true',
-            'data-modal-disclaimer': 'This is a custom LocalStack LLM to help you find the information you need by searching across all LocalStack documentation. Give it a try and let us know what you think!',
+            'data-modal-disclaimer':
+              'This is a custom LocalStack LLM to help you find the information you need by searching across all LocalStack documentation. Give it a try and let us know what you think!',
           },
         },
       ],
@@ -113,7 +119,7 @@ export default defineConfig({
           showCaptions: true,
         }),
         starlightTocOverviewCustomizer({
-          overviewTitle: "Back to top",
+          overviewTitle: 'Back to top',
         }),
         starlightFullViewMode({
           leftSidebarEnabled: false,
