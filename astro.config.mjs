b27199fcad8b6f9cb8ce17dd064cf1265b018647
Brian Rinaldi <brian.rinaldi@gmail.com>
2026-01-30 07:48:13 -0500

    Add a persistent announcement across AWS docs (#393)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import starlightLinksValidator from 'starlight-links-validator';
 import starlightImageZoom from 'starlight-image-zoom';
 import sitemap from '@astrojs/sitemap';
 import starlightFullViewMode from 'starlight-fullview-mode';
-import starlightTocOverviewCustomizer from 'starlight-toc-overview-customizer';
 
 import markdoc from '@astrojs/markdoc';
 
@@ -20,7 +19,7 @@ const response = await fetch(
   'https://api.github.com/repos/localstack/localstack/releases/latest',
   {
     headers: { Accept: 'application/vnd.github+json' },
-  }
+  },
 );
 const data = await response.json();
 const latestVersion = data.tag_name.replace('v', '');
@@ -43,6 +42,7 @@ export default defineConfig({
     starlight({
       title: 'Docs',
       favicon: '/images/favicons/favicon.ico',
+      routeMiddleware: './src/routeData.ts',
       customCss: [
         './src/fonts/font-face.css',
         './src/styles/global.css',
@@ -54,6 +54,7 @@ export default defineConfig({
       components: {
         PageSidebar: './src/components/PageSidebarWithBadges.astro',
         LanguageSelect: './src/components/LanguageSelectWithGetStarted.astro',
+        Banner: './src/components/BannerWithPersistentAnnouncement.astro',
       },
       expressiveCode: {
         themes: ['one-light', 'one-dark-pro'],
@@ -188,9 +189,6 @@ export default defineConfig({
       plugins: [
         starlightImageZoom({
           showCaptions: true,
-        }),
-        starlightTocOverviewCustomizer({
-          overviewTitle: 'Back to top',
         }),
         starlightFullViewMode({
           leftSidebarEnabled: false,
