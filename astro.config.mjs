5b0b1ceea6fd024a78528a73a6cee779499a9525
Brian Rinaldi <brian.rinaldi@gmail.com>
2026-05-26 09:48:25 -0400

    Remove the Reo integration script from Astro (#663)

--- astro.config.mjs
+++ astro.config.mjs
@@ -251,13 +251,6 @@ export default defineConfig({
           tag: 'script',
           attrs: {
             type: 'text/javascript',
-          },
-          content: `!function(){var e,t,n;e="f528d4d390d322d",t=function(){Reo.init({clientID:"f528d4d390d322d"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.defer=!0,n.onload=t,document.head.appendChild(n)}();`,
-        },
-        {
-          tag: 'script',
-          attrs: {
-            type: 'text/javascript',
             id: 'icon-script-loader',
             // defer only: async would win and delay/unorder execution vs parse-complete
             defer: true,
